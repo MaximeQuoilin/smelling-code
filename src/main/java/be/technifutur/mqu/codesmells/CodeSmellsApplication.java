24ca22c987cd338f3bfb0ecb6f869c5c25b5b46a
Maxime Quoilin <max.quoilin@gmail.com>
2019-10-28 11:23:01 +0100

    first commit

---
+++ src/main/java/be/technifutur/mqu/codesmells/CodeSmellsApplication.java
@@ -0,0 +1,13 @@
+package be.technifutur.mqu.codesmells;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSmellsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeSmellsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/technifutur/mqu/codesmells/CodeSmellsApplicationTests.java
@@ -0,0 +1,13 @@
+package be.technifutur.mqu.codesmells;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSmellsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
